792234d738687a2a280e8d0c856f63d8167ef9a8
Wil Erickson <william@ericksonls.com>
2023-09-28 11:47:35 -0700

    added cplorations and completed c00

---
+++ cplorations/c00/main.c
@@ -0,0 +1,13 @@
+/**************************************** 
+ * C-ploration 0 for CS 271 
+ *   
+ * [NAME] $YOUR_NAME$  
+ * [TERM] FALL $YEAR$  
+ *****************************************/
+#include <stdio.h>
+int main() {
+   // printf(.) displays the string given
+   printf("Hello world!\n");
+   // main returns 0 if exiting without error
+   return 0;
+}
