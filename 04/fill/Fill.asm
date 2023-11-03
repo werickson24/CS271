c4ce1e2ae30bc8e7d3b5f2ba6671d8134358d257
Wil Erickson <william@ericksonls.com>
2023-11-02 18:38:46 -0700

    Finished fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,74 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+(INPUTLOOP)
+
+
+//store the screen address for manipulation
+@SCREEN
+D=A
+@screenaddress
+M=D
+
+//end address
+@8191
+D=A
+@screenaddress
+D=D+M
+@screenend
+M=D
+
+//jump to blackout and skip whiteout routine
+@KBD
+D=M
+@BLACKOUT
+D;JEQ
+
+(SCREENFILL)
+
+@screenaddress
+A=M //dereference address to value
+M=0
+
+//increment address to the next 16 bit address
+@1
+D=A
+@screenaddress
+M=D+M
+D=M
+
+//end if we've reached 8000 into the screen
+@screenend
+D=M-D
+
+@SCREENFILL
+D;JGE
+
+@INPUTLOOP
+0;JMP
+
+(BLACKOUT)
+
+@screenaddress
+A=M //dereference address to value
+M=-1
+
+//increment address to the next 16 bit address
+@1
+D=A
+@screenaddress
+M=D+M
+D=M
+
+//end if we've reached 8000 into the screen
+@screenend
+D=M-D
+
+@BLACKOUT
+D;JGE
+
+
+@INPUTLOOP
+0;JMP
