7c16619adcab2a496a640f80361dcfeedd85dfba
Wil Erickson <william@ericksonls.com>
2023-11-10 20:37:36 -0800

    create cploration 7 and update various files

---
+++ cplorations/c07/parser.c
@@ -0,0 +1,110 @@
+/****************************************
+ * C-ploration 4 for CS 271
+ * 
+ * [NAME] $William Erickson$
+ * [TERM] FALL $2023$
+ * 
+ ****************************************/
+#include "parser.h"
+
+/* Function: strip
+ * -------------
+ * remove whitespace and comments from a line
+ *
+ * s: the char* string to strip
+ *
+ * returns: the stripped char* string
+ */
+char *strip(char *s){	
+
+	char s_new[strlen(s) + 1];
+	
+	int i = 0;
+	
+	for(char *s2 = s; *s2; s2++){
+		if(*s2 == '/' && *(s2 + 1) == '/'){
+			break;
+		}else if(isspace(*s2)){
+			continue;
+		}else{
+			s_new[i++] = *s2;
+		}
+		//printf("processing char %c at index %d", *s2, i);
+	}
+	
+	s_new[i] = '\0';
+	
+	strcpy(s, s_new);
+	
+    return s;	
+}
+
+/* Function: parse
+ * -------------
+ * iterate each line in the file and strip whitespace and comments. 
+ *
+ * file: pointer to FILE to parse
+ *
+ * returns: nothing
+ */
+void parse(FILE * file){
+	
+	char line[MAX_LINE_LENGTH];
+	
+	while (fgets(line, sizeof(line), file) != NULL){
+		
+		strip(line);
+		
+		if(*line){
+			char inst_type = is_Atype(line) ? 'A' : '\0';
+			inst_type = is_label(line) ? 'L' : inst_type;
+			inst_type = is_Ctype(line) ? 'C' : inst_type;
+			printf("%c  ", inst_type);
+			
+			if(inst_type == 'L'){
+				char label_line[MAX_LABEL_LENGTH];
+				extract_label(line, label_line);
+				printf("%s\n", label_line);
+			}else{
+				printf("%s\n", line);
+			}
+		}
+		
+	}
+	
+}
+
+bool is_Atype(const char *line){
+	char Astart = '@';
+	return (*line == Astart);
+}
+
+bool is_label(const char *line){
+	int end_index = strlen(line) - 1;
+	if(*line == '(' && *(line + end_index) == ')'){
+		return true;
+	}else{
+		return false;
+	}
+	
+}
+
+bool is_Ctype(const char *line){
+	if(!is_Atype(line) && !is_label(line)){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+char *extract_label(const char *line, char* label){
+	char buffer[MAX_LABEL_LENGTH];
+	int i = 0;
+	while(line[i + 1] != ')' && i < MAX_LABEL_LENGTH){
+		buffer[i] = line[i + 1];
+		i++;
+	}
+	buffer[i] = '\0';
+	strcpy(label, buffer);
+	return label;
+}
---
+++ cplorations/c07/parser.h
@@ -0,0 +1,63 @@
+/****************************************
+ * C-ploration 7 for CS 271
+ * 
+ * [NAME] $William Erickson$
+ * [TERM] FALL $2023$
+ * 
+ ****************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define MAX_LINE_LENGTH  200
+#define MAX_LABEL_LENGTH (MAX_LINE_LENGTH - 2)
+
+enum instr_type{
+	invalid = -1,
+	atype,
+	ctype
+};
+
+typedef int16_t hack_addr;
+typedef int16_t opcode;
+typedef struct c_instruction{
+	opcode a:1;
+	opcode comp:7;
+	opcode dest:3;
+	opcode jump:3;
+} c_instruction;
+
+typedef struct a_instruction{
+	
+	union atypes{
+		hack_addr address;
+		char * label;
+	} atypes;
+	
+	bool is_addr;
+	
+} a_instruction;
+
+typedef struct instruction{
+	union instrs{
+		a_instruction ainst;
+		c_instruction cinst;
+	} atypes;
+	enum instr_type type;
+} instruction;
+
+/** function prototypes **/
+char *strip(char *s);
+
+void parse(FILE * file);
+
+bool is_Atype(const char *line);
+
+bool is_label(const char *line);
+
+bool is_Ctype(const char *line);
+
+char *extract_label(const char *line, char* label);
