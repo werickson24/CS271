2089c07c5ca200c2344641ed19a61a40de8b98a9
Wil Erickson <william@ericksonls.com>
2023-11-21 11:57:17 -0800

    add error.h

--- cplorations/c08/error.h
+++ cplorations/c08/error.h
@@ -1,4 +1,16 @@
 #define __ERROR_H__
 #ifndef __ERROR_H__
 
+#include <stdarg.h>
+
+typedef enum exitcode{
+	/*
+	* Error code 1 represents that given file does not exist.
+	*/
+	EXIT_INCORRECT_ARGUMENTS = 1,
+	
+} exitcode;
+
+void exit_program(enum exitcode code, ...);
+
 #endif
